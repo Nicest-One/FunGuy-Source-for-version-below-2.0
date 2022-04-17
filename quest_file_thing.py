4fdbc919562dd7cb2bb9018871c03195689202b0
Nice One <101986923+Nicest-One@users.noreply.github.com>
2022-04-17 17:34:17 -0400

    Create quest_file_thing.py

---
+++ quest_file_thing.py
@@ -0,0 +1,13 @@
+
+#quest id system
+
+
+all_quests_dict = {
+  '1': {'action': 'fish', 'times': 1, 'tip':'Type F!fish to complete this task'},
+  '2': {'action': 'mine', 'times': 2, 'tip':'Type F!mine to complete this task'},
+  '3': {'action': 'daily', 'times': 1, 'tip':'Type F!daily to complete this task'},
+  '4': {'action': 'train', 'times': 2, 'tip':'Type F!train to complete this task'},
+  '5': {'action': 'hourly', 'times': 5, 'tip':'Type F!hourly to complete this task'},
+  '6': {'action': 'dig', 'times': 4, 'tip':'Type F!dig to complete this task'}
+
+}
