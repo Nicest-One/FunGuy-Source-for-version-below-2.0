4b6b3e9dae28043b68f1d1283e4c2fcdf035a076
Nice One <101986923+Nicest-One@users.noreply.github.com>
2022-04-17 17:33:33 -0400

    Create item_dictionary_info.py

---
+++ item_dictionary_info.py
@@ -0,0 +1,8 @@
+item_info_list = [["start", "command", "Use this command to get access to other commands!", "none", "none"], ["profile", "command", "Use this command to view your profile or when **user** argument given view (user)'s profile", "(user) **|** profile (user)", "p, bal"], ["inventory", "command", "Use this command to view your inventory or when **user** argument given view (user)'s inventory", "(user) **|** inventory (user)", "i, inv"], ["quest", "command", "Use this command to view your quest, complete it and get a reward", "none", "q, quests"], ["cooldown", "command", "Use this command to view all your cooldowns on commands", "none", "cd"], ["pet", "command", "__View Your pet__\nTo see your pet type F!pet\n__Equip a new pet__\nTo equip a new pet type F!pet equip <pet_name> **|** Doing this will reset your pet level and xp!", "(action) (pet_name) **|** F!pet (action) (pet_name)", "none"], ["train", "command", "Use this command to train your pet and get xp!", "none", "none"], ["upgrade", "command", "Use this command to upgrade your pet", "none", "none"], ["farm", "command", "Use this command to grow crops! **|** More information about usage comming soon", "(action) (plant) **|** F!farm (action) (plant)", "none"], ["brew", "command", "Use this command to brew potions! **|** More information about usage comming soon", "(potion) **|** F!brew (potion)", "none"]]
+
+#format
+# 1 = command name
+# 2 = type
+# 3 = arguments
+# 4 = command desc
+# 5 = aliases
